d82c316257e45fbc5c088ebaa5674560a35b1720
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-17 15:12:17 -0800

    successfully can make an api call

--- Gemfile
+++ Gemfile
@@ -1,6 +1,13 @@
 source 'https://rubygems.org'
 
 ruby "2.1.2"
+
+group :development do
+  gem "better_errors"
+  gem "binding_of_caller"
+end
+
+
 gem "httparty"
 gem "excon"
 gem "dotenv-rails"
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,15 +2,27 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find_by(number: params[:number])
+    @shipping_info
   end
 
   def update
+    @order = Order.new(params[:id])
     order_form = OrderForm.new(current_order, params[:order])
     if order_form.save
       session[:order_number] = nil
-      
+      # @country = params[:country]
+      # @state = params[:state]
+      # @city = params[:city]
+      # @zip = params[:zip]
+      request = HTTParty.get("http://localhost:3000/?country=#{@country}&state=#{@state}&city=#{@city}&zip=#{@zip}")
+      # http://localhost:3000/quotes/search?country=US&state=MD&city=Baltimore&zip=21231
+      @shipping_info = request.parsed_response
 
-      redirect_to order_path(order_form.order.number)
+      render :update
+      @country = @order.country
+      raise
+
+      # redirect_to order_path(order_form.order.number)
     end
 
 
