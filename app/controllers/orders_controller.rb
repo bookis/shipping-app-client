1502ad0d10c8dd0a63e7202af71395b58b713309
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-17 15:19:54 -0800

    clean up api call

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,21 +6,24 @@ class OrdersController < ApplicationController
   end
 
   def update
-    @order = Order.new(params[:id])
     order_form = OrderForm.new(current_order, params[:order])
     if order_form.save
       session[:order_number] = nil
-      # @country = params[:country]
-      # @state = params[:state]
-      # @city = params[:city]
-      # @zip = params[:zip]
+
+      @order = order_form.order
+
+
+      @city = @order.address.city
+      @state = @order.address.state
+      @zip = @order.address.postal_code
+      @country = "US"
+
       request = HTTParty.get("http://localhost:3000/?country=#{@country}&state=#{@state}&city=#{@city}&zip=#{@zip}")
       # http://localhost:3000/quotes/search?country=US&state=MD&city=Baltimore&zip=21231
-      @shipping_info = request.parsed_response
 
-      render :update
-      @country = @order.country
+      @shipping_info = request.parsed_response
       raise
+
 
       # redirect_to order_path(order_form.order.number)
     end
