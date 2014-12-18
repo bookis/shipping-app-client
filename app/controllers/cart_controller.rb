0caa02c2b6ee5b0c844d37f1c7a4803be7fc29f5
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-18 12:00:00 -0800

    can display ups shipping options and prices

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -7,17 +7,20 @@ class CartController < ApplicationController
   end
 
   def show
+    if !current_order.address.nil?
+
+      @city = current_order.address.city
+      @state = current_order.address.state
+      @zip = current_order.address.postal_code
+      # FOR UPS
+      @request = HTTParty.get("http://localhost:3000/quotes/search?origincountry=US&originstate=WA&origincity=Seattle&originzip=98121&country=US&state=#{@state}&city=#{@city}&zip=#{@zip}&carrier=UPS")
+      # http://localhost:3000/quotes/search?origincountry=US&originstate=WA&origincity=Seattle&originzip=98121&country=US&state=MD&city=Baltimore&zip=21231&carrier=UPS
+
+      @ups_shipping_info = @request.parsed_response
 
 
-
-    # @city = @order.address.city
-    # @state = @order.address.state
-    # @zip = @order.address.postal_code
-    # @country = "US"
-    #
-    # request = HTTParty.get("http://localhost:3000/?country=#{@country}&state=#{@state}&city=#{@city}&zip=#{@zip}")
-    # # http://localhost:3000/quotes/search?country=US&state=MD&city=Baltimore&zip=21231
-    #
-    # @shipping_info = request.parsed_response
+      # @ups_ground_price = @shipping_info["rates"][0]["total_price"]
+      # @ups_three_day_select_price = @shipping_info["rates"][1]["total_price"]
+    end
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,23 +7,11 @@ class OrdersController < ApplicationController
 
   def update
     order_form = OrderForm.new(current_order, params[:order])
+    raise
+    redirect_to order_path(order_form.order.number)
     if order_form.save
       session[:order_number] = nil
 
-      @order = order_form.order
-
-
-      @city = @order.address.city
-      @state = @order.address.state
-      @zip = @order.address.postal_code
-      @country = "US"
-
-      request = HTTParty.get("http://localhost:3000/?country=#{@country}&state=#{@state}&city=#{@city}&zip=#{@zip}")
-      # http://localhost:3000/quotes/search?country=US&state=MD&city=Baltimore&zip=21231
-
-      @shipping_info = request.parsed_response
-
-      redirect_to order_path(order_form.order.number)
     end
 
 
