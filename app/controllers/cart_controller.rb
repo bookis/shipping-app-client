77d311e7f74c40c1c29799160653540f6b45acdd
Rachel <rmoshier@gmail.com>
2014-12-19 10:11:17 -0800

    ups stuff shows in box

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -7,5 +7,24 @@ class CartController < ApplicationController
   end
 
   def show
+    options = {
+      origin:
+        {
+          country:  'US',
+          state:  'CA',
+          city:  'Beverly Hills',
+          zip:  '90210'
+        },
+      destination:
+        {
+          country: 'US',
+          state: 'WA',
+          city:  'Seattle',
+          zip:  '98122'
+        },
+      package: { weight: 100, dimensions: [5, 7, 6] } }
+
+    # HTTParty.post("http://shipalot.herokuapp.com/ups.json", body: options.to_json, headers: {'Content-Type' => 'application/json'})
+    @estimate = HTTParty.get("http://localhost:3000/ups.json", body: options.to_json)
   end
 end
